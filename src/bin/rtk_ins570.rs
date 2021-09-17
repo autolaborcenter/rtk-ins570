1ab97bdd2b00325370e53c5aaef7af54f5305c4b
YdrMaster <ydrml@hotmail.com>
2021-09-17 17:57:15 +0800

    feat: 主目标改为一个库，方便其他项目调用

---
+++ src/bin/rtk_ins570.rs
@@ -0,0 +1,52 @@
+﻿use chrono::{DateTime, Local};
+use rtk_1ns570_rs::*;
+use std::f64::consts::PI;
+use std::io::Write;
+
+fn main() {
+    let datetime: DateTime<Local> = std::time::SystemTime::now().into();
+    let threads = RTKThreads::open_all(move |name, rtk| {
+        let file_path = format!("log/{}", datetime.format("%Y-%m-%d"));
+        let file_name = format!(
+            "{}/{}-{}.txt",
+            file_path.as_str(),
+            datetime.format("%H-%M-%S"),
+            name
+        );
+        let mut file: Option<std::fs::File> = None;
+
+        for s in rtk {
+            let ins570::SolutionData { state, enu, dir } = s;
+            let ins570::SolutionState {
+                state_pos,
+                satellites,
+                state_dir,
+            } = state;
+            let text = format!(
+                "{} {} {} {} {} {} {}",
+                enu.e,
+                enu.n,
+                enu.u,
+                dir * 180.0 / PI,
+                state_pos,
+                state_dir,
+                satellites,
+            );
+
+            if file.is_none() {
+                std::fs::create_dir_all(file_path.as_str()).unwrap();
+                file = Some(
+                    std::fs::OpenOptions::new()
+                        .append(true)
+                        .create(true)
+                        .open(format!("log/{}.path", file_name))
+                        .unwrap(),
+                );
+            }
+            write!(file.as_ref().unwrap(), "{}\n", text).unwrap();
+            println!("{}", text.as_str());
+        }
+    });
+
+    threads.join();
+}
---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+﻿pub mod ins570;
+mod serial_port;
+
+use serial_port::SerialPort;
+
+pub struct RTK {
+    ins570: ins570::Ins570,
+    port: serial_port::Port,
+}
+
+impl RTK {
+    fn new(port: serial_port::Port) -> Self {
+        RTK {
+            ins570: ins570::Ins570::new(),
+            port,
+        }
+    }
+}
+
+impl Iterator for RTK {
+    type Item = ins570::SolutionData;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::time::{Duration, Instant};
+        let mut instant = Instant::now();
+        loop {
+            let mut buffer = self.ins570.get_buf();
+            match self.port.read(&mut buffer) {
+                Some(n) => match self.ins570.notify_received(n) {
+                    ins570::Solution::Nothing => {
+                        if Instant::now().duration_since(instant) > Duration::from_millis(500) {
+                            return None;
+                        }
+                    }
+                    ins570::Solution::Uninitialized => {
+                        instant = Instant::now();
+                    }
+                    ins570::Solution::Data(data) => {
+                        return Some(data);
+                    }
+                },
+                None => return None,
+            }
+        }
+    }
+}
+
+pub struct RTKThreads(Vec<std::thread::JoinHandle<()>>);
+
+impl RTKThreads {
+    /// 打开一些串口
+    pub fn open_some<F>(paths: &[String], block: F) -> Self
+    where
+        F: 'static + Send + Clone + FnOnce(String, RTK),
+    {
+        Self(
+            paths
+                .iter()
+                .filter_map(may_open)
+                .map(|(name, port)| {
+                    let f = block.clone();
+                    std::thread::spawn(move || f(name, RTK::new(port)))
+                })
+                .collect::<Vec<_>>(),
+        )
+    }
+
+    /// 打开所有串口
+    pub fn open_all<F>(block: F) -> Self
+    where
+        F: 'static + Send + Clone + FnOnce(String, RTK),
+    {
+        Self::open_some(serial_port::Port::list().as_slice(), block)
+    }
+
+    /// 打开一个串口
+    pub fn open<F>(name: String, block: F) -> Self
+    where
+        F: 'static + Send + Clone + FnOnce(String, RTK),
+    {
+        Self::open_some(&[name], block)
+    }
+
+    /// 阻塞
+    pub fn join(self) {
+        for thread in self.0 {
+            thread.join().unwrap();
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn may_open(name: &String) -> Option<(String, serial_port::Port)> {
+    const PREFIX: &str = "Silicon Labs CP210x USB to UART Bridge (COM";
+    const PREFIX_LEN: usize = PREFIX.len();
+
+    if !name.starts_with(PREFIX) {
+        return None;
+    }
+
+    let num = &name.as_str()[PREFIX_LEN..name.len() - 1];
+    let path = format!("\\\\.\\COM{}", num);
+    match serial_port::Port::open(path.as_str()) {
+        Ok(port) => {
+            println!("reading from {}", path);
+            Some((format!("COM{}", num), port))
+        }
+        Err(e) => {
+            eprintln!("failed to open {}: {}", path, e);
+            None
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn may_open(name: &String) -> Option<(String, serial_port::Port)> {
+    match serial_port::Port::open(name.as_str()) {
+        Ok(port) => {
+            println!("reading from {}", name);
+            Some((name.clone(), port))
+        }
+        Err(e) => {
+            eprintln!("failed to open {}: {}", name, e);
+            None
+        }
+    }
+}
